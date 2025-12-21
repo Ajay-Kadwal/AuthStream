dca8536d4a0bf4a009fd4bf48d1ea0439ad26312
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-12-21 16:57:18 +0530

    Initial Commit

---
+++ AuthStream/AuthStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthStreamApp.swift
+//  AuthStream
+//
+//  Created by AJAY KADWAL on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
